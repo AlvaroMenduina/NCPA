3c90749f654e49b0cec43af34786307a9f7fffc4
AlvaroMenduina <alvaro.menduina@gmail.com>
2020-02-14 11:58:17 +0000

    Added test_nyquist_errors

--- ncpa/test_nyquist_errors.py
+++ ncpa/test_nyquist_errors.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     ### ============================================================================================================ ###
 
     # Find the Wavelength at which you have 4 mas, if you have 4 * (1 + eps) at 1.5
-    SPAX_ERR = 0.10         # Percentage of error [10%]
+    SPAX_ERR = 0.05         # Percentage of error [10%]
     WAVE_BAD = WAVE * (1 + SPAX_ERR)
     # Double-check that is the correct wavelength
     utils.check_spaxel_scale(rho_aper=RHO_APER*(1 + SPAX_ERR), wavelength=WAVE)
@@ -134,6 +134,92 @@ if __name__ == "__main__":
     # ================================================================================================================ #
     #           Impact of Nyquist Errors
     # ================================================================================================================ #
+
+    # Generate a set of PSF images with the wrong sampling
+    # Use the fake extra wavelength for which the sampling is off
+    PSF_error = psf.PointSpreadFunction(rbf_matrices[1], N_pix=N_PIX, crop_pix=pix, diversity_coef=diversity_defocus)
+
+    test_PSF_error, test_coef_error, _PSF, _coef = calibration.generate_dataset(PSF_error, 1000, 1,
+                                                                                coef_strength, rescale)
+
+    ###  WATCHOUT
+    # Calibrating here is quite tricky! Calib has a self.PSF_model the nominal model (not the one with Nyquist errors)
+    # that means that after the first iteration, the PSF images will revert to having the proper modelling
+
+    # We MUST temporarily override calib.PSF_model tem
+    calib.PSF_model = PSF_error
+
+    # In addition we need to be careful with the wavefront definition in [rad]
+    # You need to rescale the coefficients
+
+    test_PSF_error = calib.noise_effects.add_readout_noise(test_PSF_error, RMS_READ=1. / SNR)
+    RMS_evolution_errors = []
+    for i in range(N_iter):
+
+        predicted_coef = calib.cnn_model.predict(test_PSF_error)        # These are the coefficients in the "nominal" model
+        predicted_coef_nm = predicted_coef * WAVE
+        residual_nm = test_coef_error * WAVE_BAD - predicted_coef_nm
+        residual = residual_nm / WAVE
+        rms_before, rms_after = calib.calculate_RMS(predicted_coef, residual, wavelength=WAVE)
+        rms_pair = [rms_before, rms_after]
+        RMS_evolution.append(rms_pair)
+
+        test_PSF_error = calib.update_PSF(residual)
+        test_PSF_error = calib.noise_effects.add_readout_noise(test_PSF_error, RMS_READ=1. / SNR)
+
+    calib.PSF_model = PSF_nom
+
+    calib.plot_RMS_evolution(RMS_evolution_errors)
+
+    title = r'Spaxel Scale Error %.1f percent || Wavefronts [$\lambda$]' % (100*SPAX_ERR)
+    utils.show_wavefronts_grid(PSF_nom, coefs=residual_errors, rho_aper=RHO_APER, title=title)
+
+    plt.show()
+
+    # ================================================================================================================ #
+    #          See what happens as a function of Percentage Error
+    # ================================================================================================================ #
+
+    SPAX_ERR = 0.05         # Percentage of error [10%]
+    WAVE_MIN = (1 - SPAX_ERR)
+    WAVE_MAX = WAVE * (1 + SPAX_ERR)
+    N_WAVES = 11
+    # Double-check that is the correct wavelength
+    utils.check_spaxel_scale(rho_aper=RHO_APER*(1 - SPAX_ERR), wavelength=WAVE)
+    utils.check_spaxel_scale(rho_aper=RHO_APER*(1 + SPAX_ERR), wavelength=WAVE)
+
+    spax_errs = np.linspace(-SPAX_ERR, SPAX_ERR, N_WAVES, endpoint=True)
+
+    centers_multiwave = psf.actuator_centres_multiwave(N_actuators=N_actuators, rho_aper=RHO_APER, rho_obsc=RHO_OBSC,
+                                                       N_waves=N_WAVES, wave0=WAVE_MIN, waveN=WAVE_MAX, wave_ref=WAVE)
+
+    rbf_matrices = psf.actuator_matrix_multiwave(centres=centers_multiwave, alpha_pc=alpha_pc, rho_aper=RHO_APER,
+                                                 rho_obsc=RHO_OBSC, N_waves=N_WAVES,  wave0=WAVE, waveN=WAVE_BAD,
+                                                 wave_ref=WAVE, N_PIX=N_PIX)
+
+    mus_nyq, std_nyq = [], []
+    print("\nLooping over Spaxel Scale Errors")
+    for i, spaxel_error in enumerate(spax_errs):
+
+        print("\nSpaxel Error: %.1f percent" % (100 * spaxel_error))
+
+        # Generate the PSF model with the wrong Spaxel Scale
+        PSF_error = psf.PointSpreadFunction(rbf_matrices[i], N_pix=N_PIX, crop_pix=pix,
+                                            diversity_coef=diversity_defocus)
+        test_PSF_error, test_coef_error, _PSF, _coef = calibration.generate_dataset(PSF_error, 1000, 1,
+                                                                                    coef_strength, rescale)
+
+        calib.PSF_model = PSF_error
+        RMS_evolution_errors, residual_errors = calib.calibrate_iterations(test_PSF_error, test_coef_error,
+                                                                           wavelength=WAVE, N_iter=N_iter,
+                                                                           readout_noise=True, RMS_readout=1. / SNR)
+        calib.PSF_model = PSF_nom
+
+        final_RMS = RMS_evolution_errors[-1][-1]
+        avg, std = np.mean(final_RMS), np.std(final_RMS)
+        mus_nyq.append(avg)
+        std_nyq.append(std)
+
 
 
 
